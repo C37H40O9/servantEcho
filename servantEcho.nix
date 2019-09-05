cd8553b883126113e9e9170241c900aab1185f6b
Vasily Gorev <v@gorev.one>
2019-09-05 17:36:19 +0300

    added nix files and cabal conffig

---
+++ servantEcho.nix
@@ -0,0 +1,28 @@
+{ mkDerivation, aeson, async, base, bytestring, clock, containers
+, directory, extra, filepath, free, gauge, lens-aeson, logging, microlens
+, mtl, network, newtype-generics, process, random, safe-exceptions
+, servant, servant-client, servant-server, stdenv, stm, text, time
+, transformers, transformers-either, unordered-containers, uuid
+, vector, warp, yaml, wai, base-compat, lens, http-client, http-types
+, hspec-wai
+}:
+mkDerivation {
+  pname = "servantEcho";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  libraryHaskellDepends = [
+    aeson async base bytestring clock containers directory extra
+    filepath free gauge lens-aeson logging microlens mtl network
+    newtype-generics process random safe-exceptions servant
+    servant-client servant-server stm text time transformers
+    transformers-either unordered-containers uuid vector warp yaml
+    wai base-compat lens http-client http-types hspec-wai
+  ];
+  executableHaskellDepends = [ base ];
+  doHaddock = false;
+  doCheck = false;
+  description = "Test servant app";
+  license = stdenv.lib.licenses.bsd3;
+}
---
+++ shell.nix
@@ -0,0 +1,46 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc865", doBenchmark ? false }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, aeson, async, base, bytestring, clock
+      , containers, directory, extra, filepath, free, lens-aeson, logging
+      , microlens, mtl, network, newtype-generics, process, random
+      , safe-exceptions, servant, servant-client, servant-server, stdenv
+      , stm, text, time, transformers, transformers-either
+      , unordered-containers, uuid, vector, warp, yaml, wai
+      , base-compat , lens , http-client , http-types , hspec-wai
+      }:
+      mkDerivation {
+        pname = "servantEcho";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        libraryHaskellDepends = [
+          aeson async base bytestring clock containers directory extra
+          filepath free lens-aeson logging microlens mtl network
+          newtype-generics process random safe-exceptions servant
+          servant-client servant-server stm text time transformers
+          transformers-either unordered-containers uuid vector warp yaml
+          wai base-compat lens http-client http-types hspec-wai
+        ];
+        executableHaskellDepends = [ base ];
+        doHaddock = false;
+        doCheck = false;
+        description = "Test servant app";
+        license = stdenv.lib.licenses.bsd3;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
