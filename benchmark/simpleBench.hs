ab322bf2ba67b84d28ea5aefa647e299ac5ac435
Vasily Gorev <v@gorev.one>
2019-09-06 16:15:36 +0300

    added benchs with wrk tool

--- benchmark/simpleBench.hs
+++ benchmark/simpleBench.hs
@@ -1,11 +1,24 @@
 
+import Control.Concurrent
 import Gauge
+import System.Process (shell, readCreateProcess)
 import WebApp
+
+runCmd cmd = readCreateProcess (shell cmd) ""
+
+runWRK route = runCmd $ "wrk -t2 -c5 -d5s -H 'Host: example.com' --timeout 2s http://localhost:8080" ++ route
 
 action = pure ()
 
 main :: IO ()
 main = do
+    -- running the application and workload generator on the same machine is not a good idea
+    tId <- forkIO runApp
+    putStrLn "Installed wrk required"
+    putStrLn =<< runWRK "/echo/hi"
+    putStrLn =<< runWRK "/swagger.json"
+    putStrLn =<< runWRK "/servant-docs.md"
+    killThread tId
     defaultMain
         [ bench "Pure unit" $ nfIO $ action
         ]
